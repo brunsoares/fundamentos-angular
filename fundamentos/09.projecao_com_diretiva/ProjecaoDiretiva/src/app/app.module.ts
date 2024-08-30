f701028f849834ec464893604658b5bff07be641
brunsoares <brunohenrique.soares@outlook.com>
2024-08-29 21:50:05 -0300

    - Diretivas

--- fundamentos/09.projecao_com_diretiva/ProjecaoDiretiva/src/app/app.module.ts
+++ fundamentos/09.projecao_com_diretiva/ProjecaoDiretiva/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardTextTitleDirective } from './components/card/directives/card-text-title.directive';
+import { CardTextSubtitleDirective } from './components/card/directives/card-text-subtitle.directive';
+import { CardTextDescriptionDirective } from './components/card/directives/card-text-description.directive';
+import { CardAvatarSmallDirective } from './components/card/directives/card-avatar-small.directive';
+import { CardAvatarLargeDirective } from './components/card/directives/card-avatar-large.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardTextTitleDirective,
+    CardTextSubtitleDirective,
+    CardTextDescriptionDirective,
+    CardAvatarSmallDirective,
+    CardAvatarLargeDirective
   ],
   imports: [
     BrowserModule,
