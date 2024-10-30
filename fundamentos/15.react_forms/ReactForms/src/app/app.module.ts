88bf78651f999cbb46ed99d9391e872d982559cf
brunsoares <brunohenrique.soares@outlook.com>
2024-10-29 22:04:00 -0300

    - Criação do projeto de fundamentos

---
+++ fundamentos/15.react_forms/ReactForms/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Ex001Component } from './exemplos/ex001/ex001.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, Ex001Component],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ fundamentos/15.react_forms/ReactForms/src/app/exemplos/ex001/ex001.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-ex001',
+  templateUrl: './ex001.component.html',
+  styleUrl: './ex001.component.scss',
+})
+export class Ex001Component implements OnInit {
+  name: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+  // Validador customizado
+  // name: FormControl = new FormControl('', {nonNullable: true, validators: Validators.required});
+  // Atualizar o valor do input apenas no evento change
+  // name: FormControl = new FormControl('', {nonNullable: true, validators: Validators.required, updateOn: 'change'});
+
+  ngOnInit() {
+    this.name.valueChanges.subscribe((value) => {
+      console.log('Valor atualizado: ', value);
+    });
+  }
+
+  showStatus() {
+    if (this.name.valid) {
+      alert('Nome válido');
+    } else {
+      alert('Nome inválido');
+    }
+  }
+
+  changeValue() {
+    this.name.setValue('Novo nome atualizado');
+  }
+
+  changeInput() {
+    console.log('Valor alterado: ', this.name.value);
+  }
+
+  disableInput() {
+    this.name.disable();
+  }
+
+  enableInput() {
+    this.name.enable();
+  }
+
+  resetInput() {
+    this.name.reset();
+  }
+
+  setValidatorsInput() {
+    // Sobrescrevendo os validadores (sem o required)
+    this.name.setValidators([Validators.minLength(3)]);
+    this.name.updateValueAndValidity();
+  }
+
+  addValidatorsInput() {
+    // Adicionando validadores (sem sobrescrever os existentes)
+    this.name.addValidators([Validators.maxLength(10)]);
+    this.name.updateValueAndValidity();
+  }
+}
